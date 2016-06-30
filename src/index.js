ce24b5c3c07ffb15a3adca88aba001cce321ed0d
dustin-H <dustin.hoffner@gmail.com>
2016-06-30 23:07:17 +0200

    Using fela-stylesheet instead of doing all in this lib

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import { connect } from 'react-fela'
+import { create } from 'fela-stylesheet'
 
 export default styles => {
-  const mapStylesToProps = props => renderer => Object.keys(styles).reduce((classNames, rule) => {
-    const currentRule = styles[rule]
-
-    if (typeof currentRule !== 'function') {
-    	styles[rule] = () => currentRule
-    }
-
-    classNames[rule] = renderer.renderRule(styles[rule])
-    return classNames
+  const stylesheet = create(styles)
+  const mapStylesToProps = props => renderer => Object.keys(stylesheet).reduce((rules, rule) => {
+    rules[rule] = renderer.renderRule(rules[rule], props)
+    return rules
   }, {})
   return connect(mapStylesToProps)
 }
